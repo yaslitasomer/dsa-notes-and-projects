4e50245e85ee0216f68ad4574ad9a6282528acb8
yaslitasch <omer.yaslitas@ug.bilkent.edu.tr>
2025-09-29 00:42:15 +0300

    WIP: 01-Introduction-to-Cpp module

--- lectures/01-Introduction-to-Cpp/notes_1.cpp
+++ lectures/01-Introduction-to-Cpp/notes_1.cpp
@@ -21,21 +21,22 @@ void basic_cpp_program()
 }
 
 // Class Syntax
-class IntCell{
-    public:
-        IntCell(int initialValue = 0) : storedValue( initialValue ) {}
+class IntCell
+{
+public:
+    IntCell(int initialValue = 0) : storedValue(initialValue) {}
 
-        int read() const {return storedValue;}
-        void write(int x) {storedValue = x;}
+    int read() const { return storedValue; }
+    void write(int x) { storedValue = x; }
 
-        ~IntCell() { cout << "IntCell object with storedValue of " << storedValue << " destructed" << endl; }
+    ~IntCell() { cout << "IntCell object with storedValue of " << storedValue << " destructed" << endl; }
 
-    private:
-        int storedValue;
-
+private:
+    int storedValue;
 };
 
-void intcell_function(){
+void intcell_function()
+{
     IntCell m1;
     IntCell m2(15);
     IntCell *m3;
@@ -48,7 +49,8 @@ void intcell_function(){
     m3->~IntCell();
 }
 
-void pointers(){
+void pointers()
+{
     int a, b, *p;
     a = b = 7;
     p = &a;
@@ -62,10 +64,9 @@ void pointers(){
     *p = 2 * *p - a;
 
     cout << "b = " << b << endl;
-
 }
 
-void swap_p (int *p, int *q)
+void swap_p(int *p, int *q)
 {
     int tmp = *p;
     *p = *q;
@@ -81,7 +82,7 @@ void swap_p_Call()
     cout << a << b << endl;
 }
 
-void swap_r (int &p, int &q)
+void swap_r(int &p, int &q)
 {
     int tmp = p;
     p = q;
@@ -97,14 +98,89 @@ void swap_r_Call()
     cout << a << b << endl;
 }
 
+void dynamic_memory_allocation()
+{
+    // declare a ptr to user-defined data type
+    IntCell *ptr1;
+    int *ptr2;
+
+    // dynamically allocate space for an IntCell;
+    // initialize values; return pointer and assign
+    // to ptr1
+    ptr1 = new IntCell(5);
+
+    // similar for int:
+    ptr2 = new int(2);
+    // free up the memory that ptr1 points to
+    delete ptr1;
+    // dynamically allocate array of 23
+    // IntCell slots
+    // each will be initialized to 0
+    ptr1 = new IntCell[23];
+
+    // similar for int
+    ptr2 = new int[12];
+
+    // free up the dynamically allocated array
+    delete[] ptr1;
+}
+
+void reference_variables()
+{
+    int n = 5, m = 6;
+    int &rn = n; // we should give a value to declare a reference
+
+    n = 6;
+    rn = 7;
+    cout << n << rn << m << endl;
+    rn = m; // makes n equal to m
+    cout << n << rn << m << endl;
+}
+
+void ref_vs_pointers()
+{
+    int n = 5;
+    int &rn = n;
+    int *const p = &n;
+
+    // Same Effect
+    *p = 6;
+    rn = 6;
+}
+
+void pointer_and_consts()
+{
+    // ----Constant Pointer----
+    int i = 0;
+    int *const p = &i;
+
+    //p = &j // error
+    cout << i << endl;
+    *p = 6;
+    cout << i << endl;
+
+    // ----Pointer to a Const variable----
+    i = 0;
+    const int *q = &i;
+
+    //*p = 6 // not possible
+    cout << *q << endl;
+    i = 6; // but possible independently
+    cout << *q << endl;
+}
+
+
 
 int main()
 {
-    //basic_cpp_program();
-    //intcell_function();
-    //pointers();
-    //swap_p_Call();
-    swap_r_Call();
+    // basic_cpp_program();
+    // intcell_function();
+    // pointers();
+    // swap_p_Call();
+    // swap_r_Call();
+    //dynamic_memory_allocation();
+    //reference_variables();
+    pointer_and_consts();
 
     return 0;
 }
